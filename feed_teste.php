613d0b3519f672d0343f2c66cf8552800ef8caea
Pezno04 <pedroeffaggian@gmail.com>
2024-11-27 19:02:17 -0300

    aparecer capa no feed

--- feed_teste.php
+++ feed_teste.php
@@ -19,10 +19,10 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edi
         <?php
             $consulta_feed = mysqli_query($conexao, "SELECT * FROM feed ORDER BY idFeed DESC;");
             $consulta_avaliacao = mysqli_query($conexao, "SELECT * FROM avaliacao;");
+            
             while ($feed = mysqli_fetch_assoc($consulta_feed)) {
-                
+
                 if ($feed['tipo'] == 'leitura') {
-                    
                     $consulta_leitura = mysqli_query($conexao, "SELECT * FROM leitura WHERE idLeitura = " . $feed['idLeitura'] . ";");
                     $leitura = mysqli_fetch_assoc($consulta_leitura);
                     $consulta_usuario = mysqli_query($conexao, "SELECT * FROM usuario WHERE idUsuario = " . $leitura['idUsuario'] . ";");
@@ -30,9 +30,40 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edi
                     $consulta_obra = mysqli_query($conexao, "SELECT * FROM obra WHERE idObra = " . $leitura['idObra'] . ";");
                     $obra = mysqli_fetch_assoc($consulta_obra);
                     
+                    $idApi = $obra['idApi'];
+
+                    // URL da API com o ID do livro
+                    $apiUrl = "https://www.googleapis.com/books/v1/volumes/" . urlencode($idApi);
+
+                    // Inicializando o cURL
+                    $curl = curl_init();
+
+                    // Configurando o cURL
+                    curl_setopt($curl, CURLOPT_URL, $apiUrl);
+                    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+                    // Executando a requisição
+                    $response = curl_exec($curl);
+
+                    // Verificando erros
+                    if ($response === false) {
+                        die('Erro na requisição: ' . curl_error($curl));
+                    }
+
+                    // Fechando o cURL
+                    curl_close($curl);
+
+                    // Decodificando a resposta JSON
+                    $livro = json_decode($response, true);
+                    
+                    $capa = $livro['volumeInfo']['imageLinks']['thumbnail'];
+                    
                     echo "<div class='feed-item'>";
                     echo "<h2>" . $feed['idFeed'] . "</h2>";
                     echo "<p>" . $usuario['nome'] . ' ' . $leitura['situacao'] . ' o livro ' . $obra['titulo'] . "</p>";
+                    if ($capa) {
+                        echo "<img src='" . $capa, ENT_QUOTES . "' alt='Capa do livro' style='width: 150px; height: auto;'>";
+                    }
                     echo "</div>";
                 }
                 
@@ -44,9 +75,40 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edi
                     $consulta_obra = mysqli_query($conexao, "SELECT * FROM obra WHERE idObra = " . $avaliacao['idObra'] . ";");
                     $obra = mysqli_fetch_assoc($consulta_obra);
                     
+                    $idApi = $obra['idApi'];
+
+                    // URL da API com o ID do livro
+                    $apiUrl = "https://www.googleapis.com/books/v1/volumes/" . urlencode($idApi);
+
+                    // Inicializando o cURL
+                    $curl = curl_init();
+
+                    // Configurando o cURL
+                    curl_setopt($curl, CURLOPT_URL, $apiUrl);
+                    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+                    // Executando a requisição
+                    $response = curl_exec($curl);
+
+                    // Verificando erros
+                    if ($response === false) {
+                        die('Erro na requisição: ' . curl_error($curl));
+                    }
+
+                    // Fechando o cURL
+                    curl_close($curl);
+
+                    // Decodificando a resposta JSON
+                    $livro = json_decode($response, true);
+                    
+                    $capa = $livro['volumeInfo']['imageLinks']['thumbnail'];
+                    
                     echo "<div class='feed-item'>";
                     echo "<h2>" . $feed['idFeed'] . "</h2>";
                     echo "<p>" . $usuario['nome'] . ' avaliou o livro ' . $obra['titulo'] . ' com ' . $avaliacao['nota'] . ' estrelas.' ."</p>";
+                    if ($capa) {
+                        echo "<img src='" . $capa, ENT_QUOTES . "' alt='Capa do livro' style='width: 150px; height: auto;'>";
+                    }
                     
                     $consulta_comentario = mysqli_query($conexao, "SELECT * FROM comentario WHERE idAvaliacao = " . $feed['idAvaliacao'] . " ORDER BY idComentario DESC;");
                     while ($comentario = mysqli_fetch_assoc($consulta_comentario)){
